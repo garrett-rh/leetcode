d40e32dc976f4b0f8ddf89337e7226dcba7a5eea
Garrett <garretth@tuta.io>
2021-12-30 15:08:26 -0500

    Did another

---
+++ easy/words_in_sentences/words_test.go
@@ -0,0 +1,12 @@
+package wordsinsentences
+
+import "testing"
+
+func TestMostWordsFound(t *testing.T) {
+	got := MostWordsFound([]string{"w jrpihe zsyqn l dxchifbxlasaehj", "nmmfrwyl jscqyxk a xfibiooix xolyqfdspkliyejsnksfewbjom", "xnleojowaxwpyogyrayfgyuzhgtdzrsyococuqexggigtberizdzlyrdsfvryiynhg", "krpwiazoulcixkkeyogizvicdkbrsiiuhizhkxdpssynfzuigvcbovm", "rgmz rgztiup wqnvbucfqcyjivvoeedyxvjsmtqwpqpxmzdupfyfeewxegrlbjtsjkusyektigr", "o lgsbechr lqcgfiat pkqdutzrq iveyv iqzgvyddyoqqmqerbmkxlbtmdtkinlk", "hrvh efqvjilibdqxjlpmanmogiossjyxepotezo", "qstd zui nbbohtuk", "qsdrerdzjvhxjqchvuewevyzlkyydpeeblpc"})
+	want := 6
+
+	if got != want {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
