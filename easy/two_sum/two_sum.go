f03dfd068ce2eb81c7eeffdbebdb7f4cf0afaba2
Garrett <garretth@tuta.io>
2022-01-02 22:45:53 -0500

    Removed some commented out code.

--- easy/two_sum/two_sum.go
+++ easy/two_sum/two_sum.go
@@ -1,23 +1,6 @@
 package iteration
 
 func TwoSum(nums []int, target int) []int {
-	// Slow slow
-	// var indexes []int
-	// for i := 0; i < len(nums); i++ {
-	// 	for idx, num := range nums {
-	// 		if idx == 0 {
-	// 			continue
-	// 		} else if nums[i]+num == target && i != idx {
-	// 			indexes = append(indexes, i)
-	// 			indexes = append(indexes, idx)
-	// 			break
-	// 		}
-	// 	}
-	// 	if len(indexes) == 2 {
-	// 		break
-	// 	}
-	// }
-
 	dict := map[int]int{}
 
 	for i, num := range nums {
